4d92a09c8f7b3054cd19eafb93a3d56dcc1bde09
Marshall Sorenson <mashall.sorenson@gmail.com>
2022-06-30 18:16:55 -0400

    tests: improve fetch datasource spec

--- test/data-source/fetchDataSource.test.ts
+++ test/data-source/fetchDataSource.test.ts
@@ -1,22 +1,62 @@
-import { fetchDataSource, reconfigureDataSource } from '../../src/data-source'
+import { configureDataSource, fetchDataSource, reconfigureDataSource } from '../../src/data-source'
 
 import { DataSource } from 'typeorm'
 
 describe(fetchDataSource, () => {
   beforeEach(() => {
-    reconfigureDataSource({
+    reconfigureDataSource()
+  })
+
+  test('Should use an explicit data source', async () => {
+    const explicitDataSource = new DataSource({ type: 'sqlite', database: ':memory:' })
+
+    configureDataSource({
+      dataSource: explicitDataSource,
+    })
+
+    const dataSource = await fetchDataSource()
+
+    expect(dataSource).toBeInstanceOf(DataSource)
+    expect(dataSource).toBe(explicitDataSource)
+  })
+
+  test('Should create a datasource from options', async () => {
+    configureDataSource({
+      dataSourceOptions: {
+        type: 'sqlite',
+        database: ':memory:',
+        entities: ['abcdefg'],
+      },
+    })
+
+    const dataSource = await fetchDataSource()
+
+    expect(dataSource).toBeInstanceOf(DataSource)
+    expect(dataSource.options).toEqual(
+      expect.objectContaining({
+        type: 'sqlite',
+        database: ':memory:',
+        entities: ['abcdefg'],
+      }),
+    )
+  })
+
+  test('Should create a datasource from config file', async () => {
+    configureDataSource({
       root: __dirname,
       dataSourceConfig: '../ormconfig.ts',
     })
-  })
 
-  test('Should create a data source', async () => {
     const dataSource = await fetchDataSource()
-    expect(dataSource).toBeInstanceOf(DataSource)
-  })
 
-  test('Should get an existing data source', async () => {
-    const dataSource = await fetchDataSource()
     expect(dataSource).toBeInstanceOf(DataSource)
+    expect(dataSource.options).toEqual(
+      expect.objectContaining({
+        type: 'sqlite',
+        database: ':memory:',
+        entities: ['test/entities/**/*.entity.ts'],
+        synchronize: true,
+      }),
+    )
   })
 })
